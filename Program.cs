af5b10c529a0df8ac9312b5a0f5818114034e0b5
David Shibru <davshibru@gmail.com>
2022-09-05 12:13:39 +0600

    add auth controller in auth controller add register method connected auth service and interface in Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharecterService>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 var app = builder.Build();
 
