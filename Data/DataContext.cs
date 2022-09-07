4c4c9c2186d2dbd2049e73daedfc2d70f34cc9dd
David Shibru <davshibru@gmail.com>
2022-09-07 11:45:18 +0600

    Add Skill model Created table Skill in database

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -16,5 +16,6 @@ namespace dotnet_rpg.Data
         public DbSet<Charecter> Character => Set<Charecter>();
         public DbSet<User> Users {get; set;}
         public DbSet<Weapon> Weapons {get; set;}
+        public DbSet<Skill> Skills { get; set; }
     }
 }
---
+++ Migrations/20220907054241_Skill.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class Skill : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Damage = table.Column<int>(type: "int", nullable: false),
+                    CharecterId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Skills_Character_CharecterId",
+                        column: x => x.CharecterId,
+                        principalTable: "Character",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_CharecterId",
+                table: "Skills",
+                column: "CharecterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
